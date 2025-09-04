8dd90fa862bffa771d15f09e2b149a39b4727be3
Rachit Saxena <rachitmanit@gmail.com>
2025-09-04 09:27:56 +0530

    Add basics code

--- 01-basics/04-sorting/main.go
+++ 01-basics/04-sorting/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 func main() {
@@ -17,7 +18,22 @@ func main() {
 	}
 
 	fmt.Printf("Unique Words: %v", len(words))
-	for key := range words {
-		println(key)
+
+	type kv struct {
+		k string
+		v int
+	}
+
+	var ss []kv
+	for k, v := range words {
+		ss = append(ss, kv{k, v})
+	}
+
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].v > ss[j].v
+	})
+
+	for _, word := range ss {
+		fmt.Println(word.k, "appears", word.v, "times")
 	}
 }
